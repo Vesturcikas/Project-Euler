0607a631834e027134c4400a4945aadda60f90c9
Vesturcikas <vestas1976@gmail.com>
2019-04-16 23:38:00 +0300

    problem-6

---
+++ Problem-6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----
+            //The sum of the squares of the first ten natural numbers is, 1^2 + 2^2 + ... +10^2 = 385
+            //The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)^2 = 552 = 3025
+            //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 
+            //3025 − 385 = 2640.
+            //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+            //-----
+
+            int sum1 = 0;
+            int sum2 = 0;
+            Console.WriteLine("Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.");
+            for (int i = 1; i <= 100; i++)
+            {
+                sum1 = sum1 + (int)(Math.Pow(i, 2));
+                sum2 += i;
+            }
+            sum2 = (int)(Math.Pow(sum2, 2));
+            Console.WriteLine("The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum: {0}.", sum2-sum1);
+        }
+    }
+}
