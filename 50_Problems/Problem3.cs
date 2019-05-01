fe570a820c0e9bedc59bc30ebcc5178f92165932
Vesturcikas <vestas1976@gmail.com>
2019-05-01 13:03:06 +0300

    del

---
+++ 50_Problems/Problem3.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50_Problems
+{
+    class Problem3
+    {
+        //-----
+        //The prime factors of 13195 are 5, 7, 13 and 29.
+        //What is the largest prime factor of the number 600851475143 ?
+        //-----
+
+        List<long> primenumbers = new List<long>();
+
+        Console.WriteLine("Enter the number greater than 1.");
+            Console.Write("Your number: ");
+            long givennumber = Convert.ToInt64(Console.ReadLine());
+        var last = givennumber;
+
+            for (long i = 2; i <= last; i++)
+            {
+                if (last%i==0)
+                {
+                    if (primenumbers==null)
+                    {
+                        primenumbers.Add(i);
+                        last = last / i;
+                        Console.WriteLine(i);
+                    }
+                    else
+                    {
+                        int sum = 0;
+                        for (int k = 0; k<primenumbers.Count; k++)
+                        {
+                            if (i%primenumbers[k]==0)
+                            {
+                                sum += 1;
+                            }
+                        }
+                        if (sum==0)
+                        {
+                            primenumbers.Add(i);
+                            last = last / i;
+                            Console.WriteLine(i);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine("Max: "+ primenumbers.Max());
+    }
+}
---
+++ 50_Problems/Problem5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _50_Problems
+{
+    class Problem5
+    {
+        //-----
+        //2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+        //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
+        //-----
+    }
+}
